dc967af3011b466cc0fbd5b81d44ec0a12493251
Adrianxx1 <triaxis12q@gmail.com>
2023-11-15 22:24:54 -0600

    commit 1

---
+++ src/countingsort/CountingSort.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package countingsort;
+
+/**
+ *
+ * @author Adrian
+ */
+public class CountingSort {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void countingSort(int[] array) {
+         int max = getMaxValue(array);
+         int[] count = new int[max + 1];
+         int[] sortedArray = new int[array.length];
+    }
+    
+}
